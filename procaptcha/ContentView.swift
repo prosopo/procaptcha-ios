15af58b924d82292422c3334422ec97ca8653390
George Oastler <goastler4@gmail.com>
2024-09-29 12:54:32 +0100

    console.log to xcode console

--- procaptcha/ContentView.swift
+++ procaptcha/ContentView.swift
@@ -4,18 +4,57 @@ import WebKit
 struct WebView: UIViewRepresentable {
     let url: URL
     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         let request = URLRequest(url: url)
         webView.load(request)
         
         
+        // Inject JavaScript to redirect console.log to the user content controller
+        let scriptSource = """
+        window.console.log = (function(log) {
+            return function() {
+                var message = Array.from(arguments).join(' ');
+                log.apply(console, arguments);
+                window.webkit.messageHandlers.consoleLog.postMessage(message); // Send message to Swift
+            }
+        })(console.log);
+        """
+
+        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+        userContentController.addUserScript(script) // Add the script to the user content controller
         
         return webView
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
         // Handle any updates to the view if necessary
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            if message.name == "consoleLog", let messageBody = message.body as? String {
+                print("JavaScript Console: \(messageBody)") // Print console logs to Xcode console
+            }
+        }
+        
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            if navigationAction.navigationType == .linkActivated {
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        }
     }
 }
 
