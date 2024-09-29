d332364fe756fafb2ff14343a078c8108643e8cc
George Oastler <goastler4@gmail.com>
2024-09-29 13:58:59 +0100

    html string

--- procaptcha/ContentView.swift
+++ procaptcha/ContentView.swift
@@ -2,50 +2,50 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let url: URL
+    let htmlString: String
     
     func makeCoordinator() -> Coordinator {
         return Coordinator(self)
     }
-
+    
     func makeUIView(context: Context) -> WKWebView {
         let webViewConfiguration = WKWebViewConfiguration()
         webViewConfiguration.preferences.javaScriptEnabled = true
-        
-        // Create a user content controller to handle messages
+
+        // Set up user content controller for message handling (logging)
         let userContentController = WKUserContentController()
-        userContentController.add(context.coordinator, name: "consoleLog") // Register the consoleLog handler
+        userContentController.add(context.coordinator, name: "consoleLog")
+
+        // Inject JavaScript to override console.log
+        let scriptSource = """
+        window.console.log = (function(log) {
+            return function() {
+                var message = Array.from(arguments).join(' ');
+                log.apply(console, arguments);
+                window.webkit.messageHandlers.consoleLog.postMessage(message);
+            }
+        })(console.log);
+        """
+        
+        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+        userContentController.addUserScript(script)
         
         // Add the user content controller to the configuration
         webViewConfiguration.userContentController = userContentController
         
         let webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
         webView.navigationDelegate = context.coordinator
-        let request = URLRequest(url: url)
-        webView.load(request)
         
-        
-        // Inject JavaScript to redirect console.log to the user content controller
-        let scriptSource = """
-        window.console.log = (function(log) {
-            return function() {
-                var message = Array.from(arguments).join(' ');
-                log.apply(console, arguments);
-                window.webkit.messageHandlers.consoleLog.postMessage(message); // Send message to Swift
-            }
-        })(console.log);
-        """
-
-        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-        userContentController.addUserScript(script) // Add the script to the user content controller
+        // Load the provided HTML string
+        webView.loadHTMLString(htmlString, baseURL: nil)
         
         return webView
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        // Handle any updates to the view if necessary
+        // Handle any updates if necessary
     }
-
+    
     class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
         var parent: WebView
         
@@ -53,12 +53,14 @@ struct WebView: UIViewRepresentable {
             self.parent = parent
         }
         
+        // This is where JavaScript messages are received
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
             if message.name == "consoleLog", let messageBody = message.body as? String {
-                print("JavaScript Console: \(messageBody)") // Print console logs to Xcode console
+                print("JavaScript Console: \(messageBody)") // Log to Xcode console
             }
         }
         
+        // Handle navigation actions
         func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
             if navigationAction.navigationType == .linkActivated {
                 decisionHandler(.cancel)
@@ -72,7 +74,24 @@ struct WebView: UIViewRepresentable {
 struct ContentView: View {
     var body: some View {
         VStack {
-            WebView(url: URL(string: "https://www.printplanet.de/news/signup")!)
+            WebView(htmlString: """
+            <html>
+                <head>
+                    <title>Procaptcha Demo</title>
+                    <script type="module" src="https://js.prosopo.io/js/procaptcha.bundle.js" async defer></script>
+                </head>
+                <body>
+                    <form action="" method="POST">
+                        <input type="text" name="email" placeholder="Email" />
+                        <input type="password" name="password" placeholder="Password" />
+                        <div class="procaptcha" data-sitekey="5FWCbfR7pH9QiZqLgmm5Rw4QbFwyU5EaMqUV4G6xrvrTZDtC
+"></div>
+                        <br />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </body>
+            </html>
+                """)
             // WebView(url: URL(string: "https://prosopo.io")!)
                 .frame(height: 400)
             
