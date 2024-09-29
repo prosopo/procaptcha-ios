9577456ce0763fad706dc15a4eccb6844dd61bf2
George Oastler <goastler4@gmail.com>
2024-09-29 11:38:41 +0100

    init project

---
+++ Procaptcha/ProcaptchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcaptchaApp.swift
+//  Procaptcha
+//
+//  Created by George Oastler on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProcaptchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
